edea4dd38e24a0363036c68aeac704b226f2e540
Keith Bentrup <keithbentrup@gmail.com>
2017-08-18 20:35:25 -0400

    increase limit of activity history; and switch from 'tail -r' to tac

--- find-and-delete-old-inactive-envs.sh
+++ find-and-delete-old-inactive-envs.sh
@@ -7,9 +7,9 @@
 # set -x
 
 for proj in $(~/.magento-cloud/bin/magento-cloud projects --pipe); do
-  deactivated_log=$(~/.magento-cloud/bin/magento-cloud activity:list -p $proj -e master -a --limit=150 2>/dev/null| grep 'deactivated environment' | awk '{print $4 " " $(NF-1)}')
-  # reverse order with 'tail -r' so inactive child envs deleted first 
-  for inactive_env in $(~/.magento-cloud/bin/magento-cloud environments -p $proj 2>/dev/null | grep Inactive | awk '{print $2}' | tail -r); do
+  deactivated_log=$(~/.magento-cloud/bin/magento-cloud activity:list -p $proj -e master -a --limit=500 2>/dev/null| grep 'deactivated environment' | awk '{print $4 " " $(NF-1)}')
+  # reverse order with tac so inactive child envs deleted first 
+  for inactive_env in $(~/.magento-cloud/bin/magento-cloud environments -p $proj 2>/dev/null | grep Inactive | awk '{print $2}' | tac); do
     deactivated_date=$(echo "${deactivated_log}" | sed -n "s/ $inactive_env\$//p" | head -1)
     #echo "$proj $inactive_env $deactivated_date"
     days_since_deactivation=$(echo "( $(date +%s) - $(date -d "${deactivated_date}" +%s) ) / (24*60*60)" | bc -l | awk '{printf "%.0f", $1}')
