a9155808e92ebc0c12bc93c75d5bc4e71e9eb606
Keith Bentrup <keith@keithbentrup.com>
2018-02-22 22:38:50 -0500

    updates

---
+++ magento-cloud-manager/client/src/SelectedProjects.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function SelectedProjects(props) {
+  const { projects } = props;
+
+  const projectRows = projects.map((project, idx) => (
+    <tr key={idx} onClick={() => props.onProjectClick(idx)}>
+      <td>{project.id}</td>
+      <td className="right aligned">{project.title}</td>
+    </tr>
+  ));
+
+  return (
+    <table className="ui selectable structured large table striped">
+      <thead>
+        <tr>
+          <th colSpan="5">
+            <h3>Selected Projects</h3>
+          </th>
+        </tr>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {projectRows}
+      </tbody>
+     <tfoot>
+        <tr>
+          <th>Total</th>
+          <th className="right aligned" id="total-kcal">
+
+          </th>
+        </tr>
+      </tfoot>
+    </table>
+  );
+}
+
+// function sum(foods, prop) {
+//   return foods
+//     .reduce((memo, food) => parseInt(food[prop], 10) + memo, 0.0)
+//     .toFixed(2);
+// }
