18ffafec18d2f824b6458ccad64c5fcda51f9f29
Keith Bentrup <keith@keithbentrup.com>
2018-02-28 12:55:59 -0500

    updates

--- magento-cloud-manager/app/bin/mcm.js
+++ magento-cloud-manager/app/bin/mcm.js
@@ -1,20 +1,27 @@
 #!/usr/bin/env node
-
 const commander = require('commander')
+const colors = require('colors')
 
-commander
-  .version('0.1.0')
-  .arguments('<cmd> [env]')
-  .action(function(cmd, env) {
-    cmdValue = cmd
-    envValue = env
+commander.version('0.1.0')
+  .action(function (cmd) {
+    commander.outputHelp(txt => txt.replace(/(\r?\n)\r?\n/g,'$1'))
+    console.error(`\nCommand: ${colors.red(cmd)} not recognized!`)
+    process.exit(1);
   })
 
-program.parse(process.argv)
+commander
+  .command('test [env]')
+  .description('this is test1')
+  .action(() => console.log('test called'))
 
-if (typeof cmdValue === 'undefined') {
-  console.error('no command given!')
-  process.exit(1)
+commander
+  .command('test2 [env]')
+  .description('this is test2')
+  .action(() => console.log('test2 called'))
+
+commander.parse(process.argv)
+
+
+if (!process.argv.slice(2).length) {
+  commander.outputHelp();
 }
-console.log('command:', cmdValue)
-console.log('environment:', envValue || 'no environment given')
