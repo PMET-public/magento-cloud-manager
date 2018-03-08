294bb1839c328102e105a49f2fb1a1951bbaa848
Keith Bentrup <keith@keithbentrup.com>
2018-03-08 11:03:51 -0500

    updates

--- magento-cloud-manager/app/src/application.js
+++ magento-cloud-manager/app/src/application.js
@@ -79,49 +79,55 @@ exports.updateApplicationDbCheck = (project, environment = 'master') => {
 exports.updateApplicationTest = (project, environment = 'master') => {
   // use curl -I for just headers using HTTP HEAD
   // use curl -sD - -o /dev/null  for headers (-D -: dump headers to stdout) using HTTP GET
-  const cmd = `${MC_CLI} ssh -p ${project} -e "${environment}" "
-    http_status=$(curl -sI localhost | sed -n 's/HTTP\\/1.1 \\([0-9]*\).*/\\1/p')
-    test $http_status -eq 302 || exit
-    store_url=$(curl -sI localhost | sed -n 's/Location: \\(.*\\)?.*/\\1/p')
-    cat_url=$(curl -s $base_url | perl -ne 's/.*?class.*?nav-1.*?href=.([^ ]+.html).*/\\1/ and print')
-    cat_url_product_count=$(curl -s $category_url | grep 'img.*class.*product-image-photo' | wc -l)
-    cat_url_cached=$(curl $category_url -o /dev/null -s -w '%{time_total}')
-    store_url_cached=$(curl $store_url -o /dev/null -s -w '%{time_total}')
-    php bin/magento cache:flush
-    cat_url_uncached=$(curl $category_url -o /dev/null -s -w '%{time_total}')
-    php bin/magento cache:flush
-    store_url_uncached=$(curl $store_url -o /dev/null -s -w '%{time_total}')
-    cat_url_partial_cache=$(curl $category_url -o /dev/null -s -w '%{time_total}')
-    german_check=$(curl $store_url'?___store=luma_de&___from_store=default' -s | grep 'baseUrl.*de_DE' | wc -l)
-    venia_check=$(curl $store_url'?___store=venia_us&___from_store=default' -s | grep 'baseUrl.*venia' | wc -l)
-    read -r form_url form_key <<<$(curl -sL -c /tmp/myc -b /tmp/myc $store_url'/admin/' | perl -ne 's/.*var BASE_URL.*(https.*\\/).*/\\1/ and print;s/.*var FORM_KEY = .(.*).;.*/\\1/ and print')
-    admin_check=$(curl -sv -c /tmp/myc -b /tmp/myc -X POST -d 'login[username]=admin&login[password]=admin4tls4&form_key='$form_key $form_url 2>&1 | grep 'Location.*admin/dashboard' | wc -l)
+  const cmd = `${MC_CLI} ssh -p ${project} -e "${environment}" '
+    http_status=$(curl -sI localhost | sed -n "s/HTTP\\/1.1 \\([0-9]*\\).*/\\1/p")
+    test $http_status -eq 302 || { echo $http_status && exit 0; }
+    store_url=$(curl -sI localhost | sed -n "s/Location: \\(.*\\)?.*/\\1/p")
+    cat_url=$(curl -s $store_url | perl -ne "s/.*?class.*?nav-1.*?href=.([^ ]+.html).*/\\1/ and print")
+    cat_url_product_count=$(curl -s $cat_url | grep "img.*class.*product-image-photo" | wc -l)
+    cat_url_cached=$(curl $cat_url -o /dev/null -s -w "%{time_total}")
+    store_url_cached=$(curl $store_url -o /dev/null -s -w "%{time_total}")
+    php bin/magento cache:flush > /dev/null
+    cat_url_uncached=$(curl $cat_url -o /dev/null -s -w "%{time_total}")
+    php bin/magento cache:flush > /dev/null
+    store_url_uncached=$(curl $store_url -o /dev/null -s -w "%{time_total}")
+    cat_url_partial_cache=$(curl $cat_url -o /dev/null -s -w "%{time_total}")
+    german_check=$(curl "$store_url?___store=luma_de&___from_store=default" -s | grep "baseUrl.*de_DE" | wc -l)
+    venia_check=$(curl "$store_url?___store=venia_us&___from_store=default" -s | grep "baseUrl.*venia" | wc -l)
+    php bin/magento admin:user:unlock admin > /dev/null
+    rm /tmp/myc || :
+    read -r form_url form_key <<<$(curl -sL -c /tmp/myc -b /tmp/myc "$store_url/admin/" | 
+      perl -ne "s/.*var BASE_URL.*(https.*\\/).*/\\1/ and print;s/.*var FORM_KEY = .(.*).;.*/\\1/ and print")
+    admin_check=$(curl -sv -c /tmp/myc -b /tmp/myc -X POST -d "login[username]=admin&login[password]=admin4tls&form_key=$form_key" $form_url 2>&1 | 
+      grep "Location.*admin/dashboard" | wc -l)
     echo http_status $http_status
-    echo store_url $store_url
+    echo store_url_uncached $store_url_uncached
+    echo store_url_cached $store_url_cached
     echo cat_url $cat_url
     echo cat_url_product_count $cat_url_product_count
-    echo cat_url_cached $cat_url_cached
-    echo store_url_cached $store_url_cached
-    echo cat_url_uncached $cat_url_uncached 
-    echo store_url_uncached $store_url_uncached
+    echo cat_url_uncached $cat_url_uncached
     echo cat_url_partial_cache $cat_url_partial_cache
+    echo cat_url_cached $cat_url_cached
     echo german_check $german_check
     echo venia_check $venia_check
     echo admin_check $admin_check
-"`
+'`
   return exec(cmd)
     .then(execOutputHandler)
     .then( stdout => {
-      const [httpStatus, ...rest] = stdout.trim().split('\n')
-      // const result = db
-      //   .prepare(
-      //     `INSERT INTO applications_db_checks
-      //       (project_id, environment_id, catalog_product_entity_count, catalog_category_product_count, admin_user_count, store_count, order_count, last_login_customer, last_login_admin)
-      //     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
-      //   )
-      //   .run(project, environment, catalogProductEntityCount, catalogCategoryProductCount, adminUserCount, storeCount, orderCount, lastLoginCustomer, lastLoginAdmin)
-      // logger.mylog('debug', result)
-      // return result
+      const [httpStatus, storeUrlUncached, storeUrlCached, catUrl, catUrlProductCount, catUrlUncached, catUrlPartialCache,
+        catUrlCached, germanCheck, veniaCheck, adminCheck] = stdout.trim().split('\n').map(x => x.replace(/.* /,''))
+      const result = db
+        .prepare(
+          `INSERT INTO applications_tests
+            (project_id, environment_id, http_status, store_url_uncached, store_url_cached, cat_url, cat_url_product_count, 
+            cat_url_uncached, cat_url_partial_cache, cat_url_cached, german_check, venia_check, admin_check)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+        )
+        .run(project, environment, httpStatus, storeUrlUncached, storeUrlCached, catUrl, catUrlProductCount, catUrlUncached, 
+          catUrlPartialCache, catUrlCached, germanCheck, veniaCheck, adminCheck)
+      logger.mylog('debug', result)
+      return result
     })
     .catch(errorHandler)
 }
