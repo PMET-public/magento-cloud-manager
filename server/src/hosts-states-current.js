a76a0cbb24dc721cb3d9dd851bc9f13cff406a62
Keith Bentrup <keith@keithbentrup.com>
2018-06-18 16:05:15 -0400

    update sql for hosts table to excluded inactive projects & environments

--- server/src/hosts-states-current.js
+++ server/src/hosts-states-current.js
@@ -17,17 +17,30 @@ module.exports = (req, res) => {
   //     ORDER BY timestamp ASC)
   //   ORDER BY region`
   const sql = `SELECT m.host_id, region, GROUP_CONCAT(m.proj_env_id) cotenants, hs.cpus, hs.load_avg_15, 
-      cast (hs.load_avg_15 * 100 / hs.cpus as int) utilization, max(hs.timestamp) timestamp
-    FROM matched_envs_hosts m
-    LEFT JOIN
-      ( /* the most recent query from each env */
-      SELECT project_id || ':' || environment_id proj_env_id, region, cpus, load_avg_15, max(hs.timestamp) timestamp 
-      FROM hosts_states hs 
-      /* join w/ projects to get region */
-      LEFT JOIN projects p ON p.id = hs.project_id
-      GROUP BY project_id, environment_id ORDER BY timestamp DESC) hs 
-    ON hs.proj_env_id = m.proj_env_id
-    GROUP BY host_id`
+  cast (hs.load_avg_15 * 100 / hs.cpus as int) utilization, max(hs.timestamp) timestamp
+FROM matched_envs_hosts m
+LEFT JOIN
+
+(SELECT hs.*, pe.region FROM
+/* the most recent query from each env in hs */
+( SELECT project_id || ':' || environment_id proj_env_id, cpus, load_avg_15, max(timestamp) timestamp 
+FROM hosts_states
+GROUP BY project_id, environment_id ORDER BY timestamp DESC) hs
+LEFT JOIN 
+/* projects' environments with regions*/
+(SELECT e.id environment_id, project_id, region 
+ FROM environments e 
+ LEFT JOIN projects p ON p.id = e.project_id
+ WHERE e.active = 1 
+  AND e.missing = 0
+  AND p.active = 1
+ ) pe 
+ON pe.project_id || ':' || pe.environment_id = proj_env_id
+WHERE pe.region is not null) hs
+
+ON hs.proj_env_id = m.proj_env_id
+WHERE timestamp is not null
+GROUP BY host_id`
   const rows = db.prepare(sql).all()
   res.json(rows)
 }
