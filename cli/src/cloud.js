d062d6736db443eaacffe154db2d945292871316
Keith Bentrup <keithbentrup@gmail.com>
2019-04-17 13:08:08 -0400

    update version info table and add last update info to css

--- cli/src/cloud.js
+++ cli/src/cloud.js
@@ -4,13 +4,13 @@ const generateCss = () => {
   // identify cotenants: currently active envs on the same host when they were last checked since
   // their boot time, # cpus, & ip address were the same at that time.
   // (ordered by region to keep hosts in the same region together when enumerated)
-  const sql = `select st.project_id, st.environment_id, st.ee_composer_version, st.app_yaml_md5, st.composer_lock_md5, max(st.timestamp)
-      from smoke_tests st, environments e, projects p
+  const sql = `select a.project_id, a.environment_id, a.ee_composer_version, a.app_yaml_md5, a.composer_lock_md5, max(a.timestamp) timestamp
+      from applications a, environments e, projects p
     where p.active = 1
       and p.region = 'demo'
       and p.id = e.project_id
-      and p.id = st.project_id 
-      and e.id = st.environment_id
+      and p.id = a.project_id 
+      and e.id = a.environment_id
       and e.active = 1
       and e.missing = 0
     group by p.id, e.id`
@@ -18,7 +18,11 @@ const generateCss = () => {
   const envVersions = db.prepare(sql).all()
   logger.mylog('debug', envVersions)
   let css = '.menu .nav-list a:not(.caret)::after { font-size: 10px; }'
+  let timestamp = 0
   envVersions.forEach(row => {
+    if (row.timestamp > timestamp) {
+      timestamp = row.timestamp
+    }
     let envHref = '/projects/' + row.project_id + '/environments/' +  row.environment_id
     css += '\n.menu .nav-list a[href="' + envHref + '"]:not(.caret)::after {'
     if (row.ee_composer_version === '2.3.1') {
@@ -28,6 +32,13 @@ const generateCss = () => {
     }
     css += '}'
   })
+  css += `\nul#environments::after {
+    color: red;
+    content: 'Version info last updated:\\A${new Date(timestamp * 1000).toGMTString()}\\A';
+    white-space: pre-wrap;
+    font-size: 0.8em;
+    font-weight: normal;
+  }`
   logger.mylog('info', css)
   return css
 }
