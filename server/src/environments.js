6e45fd66da298ebcae3653217b9c11b4405dd432
Keith Bentrup <keith@keithbentrup.com>
2019-09-17 12:25:59 -0400

    remove extra whitespace

--- server/src/environments.js
+++ server/src/environments.js
@@ -4,14 +4,14 @@ const {db} = require('../util/common')
 // perl -ne 's/^\s+`(.*)`.*/\1/ and print "$myvar.$1,\n"; /CREATE[^"]*"(..)/ and $myvar=$1' cloud.sql
 
 module.exports = (req, res) => {
-  const sql = `SELECT 
+  const sql = `SELECT
       s.id, a.app_yaml_md5, a.env_yaml_md5, a.ee_composer_version, a.composer_lock_md5, a.config_php_md5,
       s.cumulative_cpu_percent, s.not_valid_index_count, s.catalog_product_entity_count,
       s.catalog_category_product_count, s.admin_user_count, s.store_count, s.order_count, s.cms_block_count,
       s.localhost_http_status, s.store_url_uncached,
       s.store_url_cached, s.cat_url, s.cat_url_product_count, s.cat_url_uncached, s.cat_url_partial_cache,
       s.cat_url_cached, s.search_url, s.search_url_partial_cache, s.search_url_product_count,
-      s.german_check, s.venia_check, s.admin_check, s.last_cron_success, s.error_logs, s.last_deploy_log, s.cpus, 
+      s.german_check, s.venia_check, s.admin_check, s.last_cron_success, s.error_logs, s.last_deploy_log, s.cpus,
       s.utilization_start || ',' || s.utilization_end utilization_start_end, s.timestamp,
       e.project_id, e.id environment_id, e.title environment_title, e.branch_level, machine_name, last_created_at,
       e.project_id || '-' || e.id project_environment_id,
@@ -23,21 +23,21 @@ module.exports = (req, res) => {
         when s.last_login_admin < last_created_at then null
         else s.last_login_admin
       end as last_login_admin,
-      case 
+      case
         when e.missing = 1 then 'missing'
         when e.failure = 1 then 'failure'
         when e.active = 1 then 'active'
         when e.active = 0 then 'inactive'
         else 'invalid'
       end as env_status,
-      p.region, p.title project_title, p.user_list, 
-      case 
-        when p.active = 1 then 'active' 
-        else 'missing' 
-      end as proj_status, 
+      p.region, p.title project_title, p.user_list,
+      case
+        when p.active = 1 then 'active'
+        else 'missing'
+      end as proj_status,
       w.host_name, w.expiration
     FROM environments e
-    LEFT JOIN 
+    LEFT JOIN
       (select * FROM (SELECT * from smoke_tests ORDER BY id DESC) GROUP BY project_id, environment_id) AS s
       ON e.id = s.environment_id and e.project_id = s.project_id
     LEFT JOIN
