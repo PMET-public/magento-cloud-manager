1ea8684dea6572c44b9e500ec58a0c45624bc8eb
Keith Bentrup <keith@keithbentrup.com>
2019-09-17 16:30:02 -0400

    remove version # from query; account for db structure change

--- server/src/response-times.js
+++ server/src/response-times.js
@@ -1,15 +1,16 @@
 const {db} = require('../util/common')
 
 module.exports = (req, res) => {
-  const sql = `SELECT p.region, p.title, project_id, environment_id, ee_composer_version, localhost_http_status, 
+  const sql = `SELECT p.region, p.title, s.project_id, s.environment_id, a.ee_composer_version, localhost_http_status, 
       store_url_uncached, cat_url_uncached, utilization_start, utilization_end 
     FROM smoke_tests s
+    LEFT JOIN applications a on a.project_id = s.project_id 
+      AND a.environment_id = s.environment_id
     LEFT JOIN projects p ON p.id = s.project_id
-    WHERE localhost_http_status = 302 
-      AND ee_composer_version IS NOT null 
+    WHERE s.localhost_http_status = '302'
+      AND ee_composer_version IS NOT null
       AND store_url_uncached > 1 
-      AND cat_url_uncached > 1
-      AND ee_composer_version = '2.2.2'`
+      AND cat_url_uncached > 1`
   const rows = db.prepare(sql).all()
   res.json(rows)
 }
