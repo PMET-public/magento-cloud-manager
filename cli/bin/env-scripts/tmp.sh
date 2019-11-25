ce9c134f3ad76d4df526df83b8cf790ee4969b89
Keith Bentrup <keith@keithbentrup.com>
2019-11-24 23:18:45 -0500

    add additional tmp 1 off scripts

--- cli/bin/env-scripts/tmp.sh
+++ cli/bin/env-scripts/tmp.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # use for 1 off cmds
-curl -s https://ipinfo.io/ip 2>/dev/null
+# curl -s https://ipinfo.io/ip 2>/dev/null
+
+# fix 2.3.3 demo upgrades
+# mysql main -sN -h database.internal -e "delete t1 FROM catalog_category_product t1 INNER JOIN catalog_category_product t2 WHERE t1.entity_id > t2.entity_id AND t1.category_id = t2.category_id and  t1.product_id = t2.product_id;"
+# php bin/magento setup:upgrade
+# php bin/magento setup:static-content:deploy
+# php bin/magento cache:flush
+# rm var/.maintenance.flag
+
+# fix broken cron
+# sed -i "/'enabled' => 0/d" /app/app/etc/env.php
