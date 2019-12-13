bf4d20b77c37101e8541b42afe56938f682d6377
Keith Bentrup <keith@keithbentrup.com>
2019-12-13 00:52:19 -0500

    update diagnose script

--- cli/bin/env-scripts/diagnose-repair-report.sh
+++ cli/bin/env-scripts/diagnose-repair-report.sh
@@ -14,6 +14,23 @@ yellow='\033[1;33m'
 no_color='\033[0m'
 cur_unix_ts=$(date +%s)
 is_cloud=$(test ! -z "$MAGENTO_CLOUD_ROUTES" && echo "true" || echo "false")
+app_dir=$(test $is_cloud = "true" && echo "/app" || echo "/var/www/magento")
+db_host=$(
+  env_php_file=$app_dir/app/etc/env.php \
+  php -r '$arr=include "$_SERVER[env_php_file]"; echo $arr["db"]["connection"]["default"]["host"];'
+)
+db_name=$(
+  env_php_file=$app_dir/app/etc/env.php \
+  php -r '$arr=include "$_SERVER[env_php_file]"; echo $arr["db"]["connection"]["default"]["dbname"];'
+)
+db_username=$(
+  env_php_file=$app_dir/app/etc/env.php \
+  php -r '$arr=include "$_SERVER[env_php_file]"; echo $arr["db"]["connection"]["default"]["username"];'
+)
+db_password=$(
+  env_php_file=$app_dir/app/etc/env.php \
+  php -r '$arr=include "$_SERVER[env_php_file]"; echo $arr["db"]["connection"]["default"]["password"];'
+)
 
 report () {
   printf "${@}" | tee -a /tmp/$cur_unix_ts-report.log
@@ -28,24 +45,20 @@ get_http_response_code() {
 }
 
 is_cron_enabled() {
-  env_php_file=$1 php -r 'error_reporting(E_ERROR|E_WARNING|E_PARSE);$arr=include "$_SERVER[env_php_file]";echo !array_key_exists("enabled", $arr["cron"]) || $arr["cron"]["enabled"] == 1  ? "true" : "false";'
+  env_php_file=$1 php -r '
+    error_reporting(E_ERROR|E_WARNING|E_PARSE);
+    $arr=include "$_SERVER[env_php_file]";
+    echo !array_key_exists("enabled", $arr["cron"]) || $arr["cron"]["enabled"] == 1  ? "true" : "false";
+  '
 }
-
-app_dir=$(
-  test -f /app/composer.json && echo '/app' ||
-  { 
-    test -f /var/www/magento/composer.json && 
-    echo '/var/www/magento' 
-  } ||
-  exit 'Magento dir not found.'
-)
 
 # check Magento version
 this_ee_composer_version=$(cat $app_dir/composer.lock | get_ee_version)
 public_ee_composer_version=$(curl -s https://raw.githubusercontent.com/magento/magento-cloud/master/composer.lock | get_ee_version)
 test $this_ee_composer_version = $public_ee_composer_version &&
-  report "This env is running the lastest public Magento version:$green $public_ee_composer_version.$no_color\n" ||
-  report "This env is not running the lastest public Magento version.$yellow env: $this_ee_composer_version, public: $public_ee_composer_version.$no_color\n"
+  report "This env is running the lastest public Magento version: $green$public_ee_composer_version$no_color.\n" ||
+  report "This env is not running the lastest public Magento version.
+    $yellowpublic: $public_ee_composer_version, env: $this_ee_composer_version.$no_color\n"
 
 # check for unfinished maintenance 
 cd $app_dir/var
@@ -61,14 +74,14 @@ test -f .deploy_is_failed &&
   report "Attempting 'php bin/magento setup:upgrade'\n" &&
   {
     cd ${app_dir}
-    php bin/magento setup:upgrade >/dev/null 2>&1;
-    report 'Tailing relevant end of install_upgrade.log:\n' && \
-    cat ${app_dir}/var/log/install_upgrade.log | \
-    perl -pe 's/\e\[\d+(?>(;\d+)*)m//g;' | \
-    grep -v '^Module ' | \
-    grep -v '^Running schema' | \
-    perl -pe 's/^/\t/' | \
-    tail
+    php bin/magento setup:upgrade >/dev/null 2>&1
+    report 'Tailing relevant end of install_upgrade.log:\n' &&
+      cat ${app_dir}/var/log/install_upgrade.log |
+      perl -pe 's/\e\[\d+(?>(;\d+)*)m//g;' |
+      grep -v '^Module ' |
+      grep -v '^Running schema' |
+      perl -pe 's/^/\t/' |
+      tail
   } ||
   report 'No failed deploy flag found.\n'
 
@@ -102,7 +115,7 @@ test $(is_cron_enabled $env_file_old) = "true" ||
       test "$(is_cron_enabled $env_file_new)" = "true" &&
       { 
         mv $env_file_new $env_file_old
-        report "cron $green fixed.$no_color Running cron ...\n"
+        report "cron ${green}fixed$no_color. Running cron ...\n"
         php bin/magento cron:run > /dev/null
       } ||
       exit "Enabling cron via regex failed."
@@ -110,10 +123,10 @@ test $(is_cron_enabled $env_file_old) = "true" ||
 last_cron=$(grep -A2 '^\[.*Launching command' /var/log/cron.log | tail -3 | tr '\n' ' ')
 last_cron_ts=$(date -d "$(echo $last_cron | perl -pe 's/^.([^\.]+).*$/\1/')" +%s)
 min_since_last_cron=$(( (cur_unix_ts - last_cron_ts) / 60 ))
-report 'Last cron'
+report 'Last cron '
 echo $last_cron | grep -q 'Ran jobs by' &&
-  report "$green succeeded " ||
-  report "$red failed "
+  report "${green}succeeded " ||
+  report "${red}failed "
 test $min_since_last_cron -lt 10 &&
   report "$green"
 report "$min_since_last_cron$no_color minutes ago.\n"
@@ -133,27 +146,88 @@ report "The past 1 min load for this host: $color$load1%%$no_color\n"
 report "The past 5 min load for this host: $color$load5%%$no_color\n"
 report "Host has $nproc cpus.\n"
 
-mysql main -sN -h database.internal -e "
-  SELECT \"not_valid_index_count\", COUNT(*) FROM indexer_state WHERE status != \"valid\";
-  SELECT \"catalog_product_entity_count\", COUNT(*) FROM catalog_product_entity;
-  SELECT \"catalog_category_product_count\", COUNT(*) FROM catalog_category_product;
-  SELECT \"admin_user_count\", COUNT(*) FROM admin_user;
-  SELECT \"store_count\", COUNT(*) FROM store;
-  SELECT \"order_count\", COUNT(*) FROM sales_order;
-  SELECT \"cms_block_count\", COUNT(*) FROM cms_block;
-  SELECT \"last_login_customer\", UNIX_TIMESTAMP(last_login_at) FROM customer_log 
-    ORDER BY last_login_at DESC limit 1;
-  SELECT \"last_login_admin\", UNIX_TIMESTAMP(logdate) FROM admin_user WHERE username != \"${magentoSIAdminUser}\"
-    ORDER BY logdate DESC limit 1;
-"
+# check indexes
+invalid_index_count=$(
+  mysql $db_name -sN -h $db_host -u $db_user -p"$db_password" \
+    -e 'SELECT COUNT(*) FROM indexer_state WHERE status != "valid";'
+)
+test $invalid_index_count -gt 0 &&
+  {
+    printf "$red$invalid_index_count$no_color invalid indexes found.\n"
+    cd $app_dir
+    php bin/magnento indexer:reset 
+    php bin/magnento indexer:reindex
+  }
 
 # last admin login
+last_admin_login=$(
+  mysql $db_name -sN -h $db_host -u $db_user -p"$db_password" \
+    -e 'SELECT UNIX_TIMESTAMP(logdate) FROM admin_user WHERE username != "sionly" ORDER BY logdate DESC limit 1;'
+)
+hr_since_last_admin_login=$(( (cur_unix_ts - last_admin_login) / 3600 ))
+days_since_last_admin_login=$(( (cur_unix_ts - last_admin_login) / 86400 ))
+last_msg=$(
+  test $days_since_last_admin_login -gt 30 && 
+    echo "$yellow$days_since_last_admin_login$no_color days ago" ||
+    {
+      test $days_since_last_admin_login -gt 1 &&
+        echo "$days_since_last_admin_login days ago" ||
+        echo "$hr_since_last_admin_login hrs ago"
+    }
+)
+report "Last admin login $last_msg.\n"
 
 # last customer login
+last_customer_login=$(
+  mysql $db_name -sN -h $db_host -u $db_user -p"$db_password" \
+    -e 'SELECT UNIX_TIMESTAMP(last_login_at) FROM customer_log ORDER BY last_login_at DESC limit 1;'
+)
+hr_since_last_customer_login=$(( (cur_unix_ts - last_customer_login) / 3600 ))
+days_since_last_customer_login=$(( (cur_unix_ts - last_customer_login) / 86400 ))
+last_msg=$(
+  test $days_since_last_customer_login -gt 30 && 
+    echo "$yellow$days_since_last_customer_login$no_color days ago" ||
+    {
+      test $days_since_last_customer_login -gt 1 &&
+        echo "$days_since_last_customer_login days ago" ||
+        echo "$hr_since_last_customer_login hrs ago"
+    }
+)
+report "Last customer login $last_msg.\n"
 
-# last http access (not curl UA)
+# var/report
+cd $app_dir/var/report
+last_var_report=$(ls -tr | tail -1)
+last_var_report_mtime=$(date -d "$(stat -c %x $last_var_report)")
+last_var_report_contents=$(perl -pe 's/\\n/\n/g;s/<pre>/<pre>\n/;s/\n(#[0-9]+)/\n  \1/g' $last_var_report)
+report "\n----Most recent ${yellow}var/report$no_color from $last_var_report_mtime: ---------\n"
+report "$last_var_report_contents"
+report '\n-----------------------------------------------------------------------\n\n'
 
-# check indexes
+# exception.log
+cd $app_dir/var/log
+recent_exceptions=$(perl -ne '/report.(ERROR|CRITICAL)/ and print' exception.log | tail -3)
+report "\n----3 latest 'CRITICAL' or 'ERROR' messages in ${yellow}exception.log$no_color: ---------\n"
+report "$recent_exceptions"
+report '\n-----------------------------------------------------------------------\n\n'
 
+# support_report.log
+cd $app_dir/var/log
+recent_support_reports=$(perl -ne '/report.(ERROR|CRITICAL)/ and print' support_report.log | tail -3)
+report "\n----3 latest 'CRITICAL' or 'ERROR' messages in ${yellow}support_report.log$no_color: ----\n"
+report "$recent_support_reports"
+report '\n-----------------------------------------------------------------------\n\n'
 
-# check services
+# recent http access (not curl UA)
+cd /var/log
+recent_access=$(perl -ne '!/ "curl\// and print' /var/log/access.log | tail -5)
+report "\n----------------------Recent ${yellow}HTTP$no_color access-------------------------------\n"
+report "$recent_access"
+report '\n-----------------------------------------------------------------------\n\n'
+
+# recent non-200 http access (not curl UA)
+cd /var/log
+recent_access=$(perl -ne '!/ "curl\// and !/HTTP\/[1-2]\.?\d? 200/ and print' /var/log/access.log | tail -5)
+report "\n----------------------Recent ${yellow}non-200 HTTP$no_color access-----------------------\n"
+report "$recent_access"
+report '\n-----------------------------------------------------------------------\n\n'
