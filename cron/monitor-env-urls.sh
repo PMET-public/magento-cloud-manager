3b6abd320685a471f583569e7a6e956ecb3a5eb5
Keith Bentrup <keith@keithbentrup.com>
2021-05-25 21:30:35 -0400

    update slack report

--- cron/monitor-env-urls.sh
+++ cron/monitor-env-urls.sh
@@ -3,27 +3,19 @@
 MCM_DIR="$( cd $(dirname $0)/.. ; pwd -P )"
 
 slack_url=$(perl -nle '/slackUrl.*:\s*"(.*)"/ and print $1' "${MCM_DIR}/cli/.secrets.json")
-old_log_file="${MCM_DIR}/cron/monitoring-results.prev.log"
-new_log_file="${MCM_DIR}/cron/monitoring-results.log"
 
-cp "${new_log_file}" "${old_log_file}" || touch "${old_log_file}"
-# filter empty lines and some stderr output that begins w/ space chars
-"${MCM_DIR}/cli/bin/mcm" env:check-web-status --quiet --all -t 0 | perl -ne '/^\S/ and print' | sort > "${new_log_file}"
-resolved_errors=$(diff "${old_log_file}" "${new_log_file}" | perl -ne 's/^</?/ and print')
-new_errors=$(diff "${old_log_file}" "${new_log_file}" | perl -ne 's/^>/!/ and print')
+report="$("${MCM_DIR}/cli/bin/mcm" env:report-web-status)"
 
-if [[ ! -z "${resolved_errors}" ]]; then
-  error_count=$(echo "${resolved_errors}" | wc -l | xargs)
-  msg=$(echo -e "\n${error_count} errors have been resolved (or have a different response).\n${resolved_errors}")
-fi
-
-if [[ ! -z "${new_errors}" ]]; then
-  error_count=$(echo "${new_errors}" | wc -l | xargs)
-  msg=$(echo -e "${msg}\n\n${error_count} new errors.\n${new_errors}")
-fi
-
-if [[ ! -z "${msg}" ]]; then
-  echo "$msg"
-  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"@here ${msg}\"}" "${slack_url}"
+if [[ ! -z "$report" ]]; then
+  echo "${report//\"/\\\"}"
+#   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"@here \`\`\`
+# ${report//\"/\\\"}
+# \`\`\`\"}" "$slack_url"
+  curl -X POST -H 'Content-type: application/json' --data "{
+          \"type\": \"mrkdwn\",
+          \"text\": \"
+            ${report//\"/\\\"}
+          \"
+  }" "$slack_url"
 fi
 
