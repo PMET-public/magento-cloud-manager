133df9b985204416fcfbed0887b3e4bba8f9058f
Keith Bentrup <keith@keithbentrup.com>
2021-06-04 17:01:53 -0400

    ensure var is not split

--- cli/bin/env-scripts/fix-redirect-for-wrong-base-url.sh
+++ cli/bin/env-scripts/fix-redirect-for-wrong-base-url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-host=$(echo $MAGENTO_CLOUD_ROUTES | base64 -d | perl -pe 's/.*(https[^"]+cloud\/).*/\1/')
+host=$(echo "$MAGENTO_CLOUD_ROUTES" | base64 -d | perl -pe 's/.*(https[^"]+cloud\/).*/\1/')
 
 bin/magento config:set web/unsecure/base_url "$host"
 bin/magento config:set web/secure/base_url "$host"
