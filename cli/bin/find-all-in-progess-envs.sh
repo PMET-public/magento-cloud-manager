7797105ca6cc47ee151abe43a17b16ec12866352
Keith Bentrup <keith@keithbentrup.com>
2021-11-01 09:21:48 -0400

    update to finding in progress envs

--- cli/bin/find-all-in-progess-envs.sh
+++ cli/bin/find-all-in-progess-envs.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-~/.magento-cloud/bin/magento-cloud multi -p $(~/.magento-cloud/bin/magento-cloud projects --pipe | perl -pe 's/\n/,/' | perl -pe 's/,$//') environments >/tmp/env-list.txt 2>&1
+set -x
+
+environments="$(~/.magento-cloud/bin/magento-cloud projects --pipe | perl -pe 's/\n/,/' | perl -pe 's/,$//')"
+
+~/.magento-cloud/bin/magento-cloud multi -p "$environments" environments >/tmp/env-list.txt 2>&1
 
 perl -i.1 -ne '/Project:|In progress/ and print' /tmp/env-list.txt
 
-perl -i.2 -pe 's/.*\((.*)\)/($prev_match eq $1 ? "$1 " : (($prev_match="$1") && ""))/e;s/^\|\s+/$prev_match:/;s/ .*//' /tmp/env-list.txt | sed '/^$/d' | xargs
+perl -i.2 -pe 's/.*\((.*)\)/($prev_match eq $1 ? "$1 " : (($prev_match="$1") && ""))/e;s/^\|\s+/$prev_match:/;s/ .*//' /tmp/env-list.txt | sed '/^$/d'
