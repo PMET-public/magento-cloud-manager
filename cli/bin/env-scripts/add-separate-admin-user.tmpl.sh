baa7825b123a07a966a35e25425c6773a511f1c7
Keith Bentrup <keith@keithbentrup.com>
2021-12-09 10:59:29 -0500

    add '-n' option to prevent cli from entering interactive mode and hanging when

--- cli/bin/env-scripts/add-separate-admin-user.tmpl.sh
+++ cli/bin/env-scripts/add-separate-admin-user.tmpl.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-php bin/magento admin:user:unlock {{magentoSIAdminUser}}
+# use `-n` to catch edge cases where magento did not fully deploy and so
+# admin:user:unlock does not exist and magento asks "did you mean admin:user:create"
+# thus hanging the script
+php bin/magento admin:user:unlock -n {{magentoSIAdminUser}} || :
 php bin/magento admin:user:create --admin-user={{magentoSIAdminUser}} --admin-password={{magentoSIAdminPassword}} \
   --admin-email=keithbentrup@gmail.com --admin-firstname=si --admin-lastname=only
 
